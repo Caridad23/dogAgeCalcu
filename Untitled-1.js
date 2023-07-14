0be747ce8419c078ad45cd33bf3914d4d9c021a0
Cari <135031118+Caridad23@users.noreply.github.com>
2023-07-14 09:16:11 -0400

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,6 @@
+
+function humanAge() {
+  var dogAge = document.getElementById("age").value;
+  var humanAgeCalc = (dogAge - 2) * 4 + 21;
+  alert("Your dog is " + humanAgeCalc + " years old in human years.");
+}
